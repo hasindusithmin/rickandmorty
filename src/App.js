b17c986f134fb60415223f04b2d7924057f7edf0
hasindusithmin <hasindusithmin64@gmail.com>
2022-02-18 21:23:40 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
+import { useState } from 'react'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
 
 function App() {
+  const [cartIsEmpty] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/*" element={<About />} />
+          <Route path="/products/:id/*" element={<ProductDetails />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/test" element={(
+            <div>
+              <h2>Test page</h2>
+            </div>
+          )} />
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+          <Route 
+            path="/checkout" 
+            element={cartIsEmpty ? <Navigate to="/products" /> : <p>checkout</p>}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
